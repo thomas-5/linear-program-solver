ff7c98f41c073072427e86510cc6436db81ba9c1
Ziyue Lin <linziyue.uw@gmail.com>
2023-09-19 16:15:06 -0400

    Updated default checked method to simplex

--- app.py
+++ app.py
@@ -55,5 +55,5 @@ def solve_linear_program():
 
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
+    port = int(os.environ.get("PORT", 5001))
     app.run(host="0.0.0.0", port=port)
